f6dd9b1ee3edc49afe8457d1fb6cf89e436e0772
srv <shashankramvyas@gmail.com>
2025-05-10 21:59:49 -0500

    Create file

---
+++ 217-ContainsDuplicate/version/java/217-ContainsDuplicate_20250510_215948.java
@@ -0,0 +1,6 @@
+// Last updated: 10/05/2025, 21:59:48
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        return Arrays.stream(nums).distinct().count() != nums.length;
+    }
+}
