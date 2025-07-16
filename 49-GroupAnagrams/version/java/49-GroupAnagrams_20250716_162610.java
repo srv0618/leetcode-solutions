4d2db5f45e94981bb01dfe8ddaafa7ebc4474221
srv <shashankramvyas@gmail.com>
2025-07-16 15:26:10 -0500

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/version/java/49-GroupAnagrams_20250716_162610.java
@@ -0,0 +1,17 @@
+// Last updated: 16/07/2025, 16:26:10
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, ArrayList<String>> map = new HashMap<>();
+  
+		for(String s: strs){
+			char[] c= s.toCharArray();
+			Arrays.sort(c);
+			String s2 = new String(c);
+			map.putIfAbsent(s2, new ArrayList<>());
+			map.get(s2).add(s);
+		}
+	
+	    return new ArrayList<>(map.values());
+
+    }
+}
